0e9ca49a6fc8bc3ace76f89c4d73760a4e2ced7b
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-09-30 03:39:40 +0530

    modify dashboard

--- src/main/java/com/speedtech_automotive/controller/DashBoardFormController.java
+++ src/main/java/com/speedtech_automotive/controller/DashBoardFormController.java
@@ -1,6 +1,7 @@
 package com.speedtech_automotive.controller;
 
 import com.speedtech_automotive.utils.Timer;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.layout.Pane;
 
@@ -10,10 +11,28 @@ import java.util.Objects;
 public class DashBoardFormController {
 
     public Pane paneTimer;
+    public Pane secondPane;
 
     public void initialize() throws IOException {
-        Pane pane = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/DashBoardForm.fxml")));
+       // Pane pane = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/view/DashBoardForm.fxml")));
 //        mainPane.getChildren().setAll(pane);
         new Timer().initialize();
+    }
+
+    public void btnOrderOnAction(ActionEvent actionEvent) {
+        setUi("");
+    }
+
+    public void btnInventryOnAction(ActionEvent actionEvent) {
+        setUi("");
+    }
+
+    public void setUi(String url){
+        try {
+            Pane pane = FXMLLoader.load(getClass().getResource("/view/"+url+".fxml"));
+            secondPane.getChildren().setAll(pane);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
