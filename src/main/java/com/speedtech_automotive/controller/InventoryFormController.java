3d7d63ac2273c638cdb57cf10d8161cd80c6e0ab
gimhana_velou <gimhana@velou.com>
2023-10-05 02:41:31 +0530

    inventory form implementation

--- src/main/java/com/speedtech_automotive/controller/InventoryFormController.java
+++ src/main/java/com/speedtech_automotive/controller/InventoryFormController.java
@@ -1,4 +1,9 @@
 package com.speedtech_automotive.controller;
 
+import javafx.event.ActionEvent;
+
 public class InventoryFormController {
+    public void saveProductOnClick(ActionEvent actionEvent) {
+
+    }
 }
