c8e93ba1885eb1b039b9fd3c71717ebf301ca7f2
gimhana_velou <gimhana@velou.com>
2023-10-25 03:01:51 +0530

    supplier form implementation done without search functionality

---
+++ src/main/java/com/speedtech_automotive/model/Supplier.java
@@ -0,0 +1,93 @@
+package com.speedtech_automotive.model;
+
+import java.sql.Date;
+
+public class Supplier {
+    String batch_id;
+    String supplierName;
+    String companyName;
+    int contact;
+    String description;
+    Date addedDate;
+
+    public Supplier(String supplierName, String companyName, int contact, String description, Date addedDate) {
+        this.batch_id = batch_id;
+        this.supplierName = supplierName;
+        this.companyName = companyName;
+        this.contact = contact;
+        this.description = description;
+        this.addedDate = addedDate;
+    }
+
+    public Supplier() {
+    }
+
+    public Supplier(String batch_id, String supplierName, String companyName, int contact, String description, Date addedDate) {
+        this.batch_id = batch_id;
+        this.supplierName = supplierName;
+        this.companyName = companyName;
+        this.contact = contact;
+        this.description = description;
+        this.addedDate = addedDate;
+    }
+
+    public String getBatch_id() {
+        return batch_id;
+    }
+
+    public void setBatch_id(String batch_id) {
+        this.batch_id = batch_id;
+    }
+
+    public String getSupplierName() {
+        return supplierName;
+    }
+
+    public void setSupplierName(String supplierName) {
+        this.supplierName = supplierName;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public int getContact() {
+        return contact;
+    }
+
+    public void setContact(int contact) {
+        this.contact = contact;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getAddedDate() {
+        return addedDate;
+    }
+
+    public void setAddedDate(Date addedDate) {
+        this.addedDate = addedDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Supplier{" +
+                "batch_id='" + batch_id + '\'' +
+                ", supplierName='" + supplierName + '\'' +
+                ", companyName='" + companyName + '\'' +
+                ", contact=" + contact +
+                ", description='" + description + '\'' +
+                ", addedDate=" + addedDate +
+                '}';
+    }
+}
